37f88099d9cd3ec93348e875081eeb429d54aa21
OMA R <omarkhouya66@gmail.com>
2023-11-19 16:22:19 +0100

    updated the display component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import Input from "./Components/Input";
 import axios from "axios";
 
 export default function App() {
-    const [City,setCity] = useState()
+    const [City,setCity] = useState('')
     const [Data,setData] = useState()
-    const [Err,setErr] = useState()
 
     useEffect(()=>{
         const getData = () => {
-            axios(`https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=1f4f6dc927578529e5b24d4e8ccec4da`)
-            .then(res=>setData(res.data)).catch(err=> {})
-            console.log(Data)
+            axios(`https://api.openweathermap.org/data/2.5/weather?q=${"Safi"}&appid=1f4f6dc927578529e5b24d4e8ccec4da`)
+            .then(res=>setData(res.data))
+            .catch(err=> {})
         }
         getData()
     },[City])
 
     return (
-        <div className="d-flex justify-content-center align-items-center" style={{minHeight:"100vh"}}>
-            <div className="border rounded p-3 mx-3">
-                <Input City={City} setCity={setCity} err={Err}/>
+        <div className="d-flex justify-content-center align-items-center" style={{minHeight:"100vh",backgroundColor: "#00A9FF"}}>
+            <div className="rounded p-3 mx-3 shadow-lg" style={{backgroundColor: "#89CFF3",width: "25rem"}}>
+                <Input City={City} setCity={setCity}/>
                 <hr />
                 <Display Data={Data}/>
             </div>
--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,14 +1,27 @@
 
 export default function Display({Data}) {
+
     return (
-        <div>
-            {
-                Data && (
-                    <>
-                        <p> {Data.main.temp- 273.15}</p>
-                    </>
-                )
-            }
+        <div className="">
+            {Data && (
+                <div className="d-flex justify-content-center">
+                    <div className="w-100">
+                        <div className="d-flex justify-content-between rounded px-2" style={{backgroundColor: "#A0E9FF"}} >
+                            <img src={`https://openweathermap.org/img/wn/${Data.weather[0].icon}.png`} alt="icon" />
+                            <p className="my-auto text-end">{Math.floor(Data.main.temp - 273.15)} ºC</p>
+                        </div>
+                        <div className="text-center rounded p-2 mt-1 d-flex flex-column align-items-center justify-content-center" style={{backgroundColor: "#CDF5FD"}} >
+                            <p className="my-auto">{Data.weather[0].main}</p>
+                            <p className="my-auto"><span className="fw-lighter">Humidity </span><span className="fw-bold">{Data.main.humidity} %</span></p>
+                        </div>
+                    </div>
+                    <div className="ms-1 rounded p-2 w-100 text-center d-flex flex-column align-items-center justify-content-center" style={{backgroundColor: "#E0F4FF"}}>
+                        <p className="my-auto">{Data.weather[0].description}</p>
+                        <p className="my-auto">wind <span className="fw-bold">{Math.floor(Data.wind.speed * 3.6)} km/h</span></p>
+                        <p className="my-auto">clouds <span className="fw-bold">{Data.clouds.all} % </span></p>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
