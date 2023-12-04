7f39e24b9490eda87576f12d1dd5a0f21062f6a6
OMA R <omarkhouya66@gmail.com>
2023-12-04 23:34:56 +0100

    getData is now async

--- src/Components/index.js
+++ src/Components/index.js
@@ -7,19 +7,26 @@ export default function MainComp() {
     const [City,setCity] = useState('')
     const [Data,setData] = useState()
 
-    useEffect(()=>{
-        const getData = () => {
-            axios(`https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=1f4f6dc927578529e5b24d4e8ccec4da`)
-            .then(res=>setData(res.data))
-            .catch(err=> {})
+    const getData =async () => {
+        try {
+            const response = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=1f4f6dc927578529e5b24d4e8ccec4da`)
+            setData(response.data)
         }
+        catch (err) {
+            console.error(err)
+        }
+    }
+    useEffect(()=>{
         getData()
     },[City])
+
     return (
-        <div className="rounded p-3 mx-3 shadow-lg" style={{backgroundColor: "#89CFF3",width: "40rem"}}>
-            <Input City={City} setCity={setCity}/>
-            <hr />
-            <Display Data={Data}/>
-        </div>           
+        <div>
+            <div className="rounded p-3 mx-3 shadow-lg" style={{backgroundColor: "#89CFF3",width: "40rem"}}>
+                <Input City={City} setCity={setCity}/>
+                <hr />
+                <Display Data={Data}/>
+            </div>
+        </div>
     )
 }
